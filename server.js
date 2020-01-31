7728320017e72a07f27b8b06894d9e5e411eefa6
karansehgal15 <karansehgal151998@gmail.com>
2020-01-31 16:18:19 +0530

    addede process port

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 xp=require('express')
 srvr=xp()
+const port=process.env.port || 2121;
 todoRoute=require('./todos/todos')
 srvr.use(xp.json());
 srvr.use(xp.urlencoded({extended:true}));
 srvr.use('/todoList',todoRoute);
 srvr.use('/public',xp.static(__dirname+"/public"));
-srvr.listen(2121);
+srvr.listen(port);
